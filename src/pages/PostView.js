78001909cd789ea4877415123d1e238288779e02
Евгений Нифантьев <nifantyev@gmail.com>
2022-01-29 21:52:29 +0300

    Реализовал компонент PostView

---
+++ src/pages/PostView.js
@@ -0,0 +1,63 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import PostsContext from '../contexts/PostsContext';
+import Post from '../components/Post';
+import NotFound from '../components/NotFound';
+
+const PostView = ({ id }) => {
+  const { posts, deletePost, refresh } = useContext(PostsContext);
+  const [post, setPost] = useState(null);
+  const [deleting, setDeleting] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (posts) {
+      setPost(posts.find((o) => o.id === id));
+    }
+  }, [posts, id]);
+
+  const handleEdit = () => {
+    navigate('edit');
+  };
+
+  const handleDelete = () => {
+    setDeleting(true);
+    deletePost(id)
+      .then(() => {
+        setDeleting(false);
+        setError(null);
+        refresh();
+        navigate('/');
+      })
+      .catch((err) => {
+        setDeleting(false);
+        setError(err.message);
+      });
+  };
+
+  return posts ? (
+    post ? (
+      <div>
+        <Post post={post} />
+        <div className="buttons">
+          <button onClick={handleEdit}>Изменить</button>
+          <button onClick={handleDelete}>Удалить</button>
+        </div>
+        {deleting && <div>Удаление...</div>}
+        {error && <div>{error}</div>}
+      </div>
+    ) : (
+      <NotFound />
+    )
+  ) : (
+    <div>Загрузка...</div>
+  );
+};
+
+PostView.propTypes = {
+  id: PropTypes.number.isRequired,
+};
+
+export default PostView;
