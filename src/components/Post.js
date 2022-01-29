cdeb4e1560b32e3f8deb18f3709aae7e8b3b1f14
Евгений Нифантьев <nifantyev@gmail.com>
2022-01-29 21:46:39 +0300

    Реализовал компонент Post

---
+++ src/components/Post.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import formatDistanceStrict from 'date-fns/formatDistanceStrict';
+import ru from 'date-fns/locale/ru';
+import PostModel from '../models/PostModel';
+
+const DateTime = ({ date }) => {
+  return <div className="date">{'' + date}</div>;
+};
+
+function withPrettyDateTime(Component) {
+  return function (props) {
+    let prettyDate;
+    let unit;
+    const seconds = (new Date() - new Date(props.date)) / 1000;
+
+    if (seconds < 3600) {
+      unit = 'minute';
+    } else if (seconds < 3600 * 24) {
+      unit = 'hour';
+    } else {
+      unit = 'day';
+    }
+    prettyDate = formatDistanceStrict(new Date(props.date), new Date(), {
+      addSuffix: true,
+      unit: unit,
+      locale: ru,
+    });
+    return <Component {...props} date={prettyDate} />;
+  };
+}
+
+const DateTimePretty = withPrettyDateTime(DateTime);
+
+const Post = ({ post, clickable = false, onClick }) => {
+  const handleClick = () => {
+    if (clickable && onClick) {
+      onClick(post.id);
+    }
+  };
+
+  return (
+    <article className="post">
+      <div className="author">
+        <img src="https://i.pravatar.cc/40" alt="" className="author__avatar" />
+        <div>
+          <a href="#/" className="author__name">
+            Ilnaz Gilyazov
+          </a>
+          <div className="author__status">
+            <div>Основатель группы</div>
+            &nbsp;&sdot;&nbsp;
+            <DateTimePretty date={post.created} />
+          </div>
+        </div>
+      </div>
+      <div
+        className={`post__text ${clickable ? ' post__text-clickable' : ''}`}
+        onClick={handleClick}
+      >
+        {post.content}
+      </div>
+      <div className="reactions">
+        <a href="#/" className="reactions__link">
+          <span className="material-icons-outlined">thumb_up</span>Нравится
+        </a>
+        <a href="#/" className="reactions__link">
+          <span className="material-icons-outlined">chat_bubble_outline</span>
+          <span>Комментировать</span>
+        </a>
+      </div>
+      <div className="comment">
+        <img
+          src="https://i.pravatar.cc/30"
+          alt=""
+          className="author__avatar author__avatar-small"
+        />
+        <input
+          type="text"
+          className="comment__input"
+          placeholder="Напишите комментарий..."
+        />
+        <div className="attachments">
+          <a href="#/" className="attachments__link">
+            <span className="material-icons-outlined">sentiment_satisfied</span>
+          </a>
+          <a href="#/" className="attachments__link">
+            <span className="material-icons-outlined">photo_camera</span>
+          </a>
+          <a href="#/" className="attachments__link">
+            <span className="material-icons-outlined">gif_box</span>
+          </a>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+Post.propTypes = {
+  post: PropTypes.instanceOf(PostModel),
+  clickable: PropTypes.bool,
+};
+
+export default Post;
