2a89899eef008486c16df0689d0a733befc36ae9
Евгений Нифантьев <nifantyev@gmail.com>
2022-01-30 12:46:36 +0300

    Обновил Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -69,29 +69,6 @@ const Post = ({ post, clickable = false, onClick }) => {
           <span>Комментировать</span>
         </a>
       </div>
-      <div className="comment">
-        <img
-          src="https://i.pravatar.cc/30"
-          alt=""
-          className="author__avatar author__avatar-small"
-        />
-        <input
-          type="text"
-          className="comment__input"
-          placeholder="Напишите комментарий..."
-        />
-        <div className="attachments">
-          <a href="#/" className="attachments__link">
-            <span className="material-icons-outlined">sentiment_satisfied</span>
-          </a>
-          <a href="#/" className="attachments__link">
-            <span className="material-icons-outlined">photo_camera</span>
-          </a>
-          <a href="#/" className="attachments__link">
-            <span className="material-icons-outlined">gif_box</span>
-          </a>
-        </div>
-      </div>
     </article>
   );
 };
